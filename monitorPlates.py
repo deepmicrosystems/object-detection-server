70e63a5e671699f0d75e4c5a923958a299951e41
alvarohurtadobo <a.hurtado.bo@gmail.com>
2019-08-12 18:14:26 -0400

    fds

--- monitorPlates.py
+++ monitorPlates.py
@@ -24,8 +24,10 @@ parser.add_argument("-k", "--kill",       type = int,  default = 0,          hel
 args = parser.parse_args()
 
 if args.kitty:
+    print("Using Kitty Model")
     kitti_model = Models.KittiModel()
 else:
+    print("Using Coco Model")
     kitti_model = Models.MSCOCOModel()
 
 # Constants
@@ -66,59 +68,61 @@ if __name__ == "__main__":
         if args.show:
             #cv2.imshow("Monitor",cv2.resize(frame,(320,240)))
             cv2.imshow("Drawing",drawing_frame)
-        
 
         print(drawing_frame.shape)
         print(detections)
         str_datetime = datetime.now().strftime("%Y%m%d_%H%M%S")
         print("Current time: ",str_datetime)
-        if len(detections["predictions"])>0:
-            detected_object = detections["predictions"][0]["class"]
+        
+        for current_detection in detections["predictions"]:
+            
+            detected_object = current_detection["class"]
             print("Found {}".format(detected_object))
-            print("Original shape: ",frame.shape)
-            y_min = PROCESS_SCALE*detections["predictions"][0]["coord"]["ymax"]
-            y_max = PROCESS_SCALE*detections["predictions"][0]["coord"]["ymin"]
-            x_min = PROCESS_SCALE*detections["predictions"][0]["coord"]["xmax"]
-            x_max = PROCESS_SCALE*detections["predictions"][0]["coord"]["xmin"]
-            print("Coordinates ", x_min, y_min, " to ", x_max, y_max)
-            found_object = frame[y_min:y_max,x_min:x_max]
-            if args.show:
-                cv2.imshow("Object",found_object)
-            print("Object shape: ",found_object.shape)
-            results = alpr.recognize_ndarray(found_object)
-            print("Results: {}".format(results))
+            if detected_object in ["car","bus","truck"]:
+                print("Original shape: ",frame.shape)
+                y_min = PROCESS_SCALE*current_detection["coord"]["ymax"]
+                y_max = PROCESS_SCALE*current_detection["coord"]["ymin"]
+                x_min = PROCESS_SCALE*current_detection["coord"]["xmax"]
+                x_max = PROCESS_SCALE*current_detection["coord"]["xmin"]
+                print("Coordinates ", x_min, y_min, " to ", x_max, y_max)
+                found_object = frame[y_min:y_max,x_min:x_max]
+                if args.show:
+                    cv2.imshow("Object",found_object)
+                print("Object shape: ",found_object.shape)
+                results = alpr.recognize_ndarray(found_object)
+                print("Results: {}".format(results))
 
-            # Save results to a dictionary:
-            print("Saving results:")
-            dictionary = {  "object_detection":detections,
-                            "openalpr_detection":results}
+                # Save results to a dictionary:
+                print("Saving results:")
+                dictionary = {  "object_detection":detections,
+                                "openalpr_detection":results}
 
-            base_name = args.output+"/{}_{}_{}_".format(str(counter).zfill(6),str_datetime,detected_object)
+                base_name = args.output+"/{}_{}_{}_".format(str(counter).zfill(6),str_datetime,detected_object)
 
-            try:
-                file_name = base_name + "{}".format(results["results"]["plate"])
-                good_plates += 1
-            except:
-                print("Did not found any information, setting a default name")
-                file_name = base_name + "none"
-                none_plates += 1
-            
-            json_name = file_name + ".json"
-            
-            full_size_image_name = file_name + "_full.jpg"
-            object_image_name = file_name + "_object.jpg"
+                try:
+                    file_name = base_name + "{}".format(results["results"]["plate"])
+                    good_plates += 1
+                except:
+                    print("Did not found any information, setting a default name")
+                    file_name = base_name + "none"
+                    none_plates += 1
+                
+                json_name = file_name + ".json"
+                
+                full_size_image_name = file_name + "_full.jpg"
+                object_image_name = file_name + "_object.jpg"
 
-            # Saving image and data:
-            cv2.imwrite(full_size_image_name,frame)
-            cv2.imwrite(object_image_name, found_object)
+                # Saving image and data:
+                cv2.imwrite(full_size_image_name,frame)
+                cv2.imwrite(object_image_name, found_object)
 
-            json_data = json.dumps(dictionary)
-            print("Writing data {}".format(json_data))
-            f = open(file_name,"w")
-            f.write(json_data)
-            f.close()
-            print("Ratio: Good {} Bad: {}".format(good_plates, none_plates))
-            counter += 1
+                json_data = json.dumps(dictionary)
+                print("Writing data {}".format(json_data))
+                f = open(file_name,"w")
+                f.write(json_data)
+                f.close()
+                print("Ratio: Good {} Bad: {}".format(good_plates, none_plates))
+                counter += 1
 
         print("Period time: {:0.2f}".format(time()-auxiliar_time))
         auxiliar_time = time()
